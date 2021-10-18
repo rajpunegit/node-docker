f11b2dc0cb1ba23c359838c94aef518aaffaee03
Rajesh <rajesh@bioenabletech.com>
2021-10-18 15:39:05 +0530

    Signed-off-by: Rajesh

---
+++ node_modules/@simplewebauthn/server/dist/helpers/generateChallenge.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+/**
+ * Generate a suitably random value to be used as an attestation or assertion challenge
+ */
+export default function generateChallenge(): Buffer;
